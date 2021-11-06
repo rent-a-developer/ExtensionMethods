6183749ccced34d75bc8e7b87fbb95810bc1a861
David Liebeherr <info@rent-a-developer.de>
2021-11-06 06:10:17 +0100

    Added extension method "ToDebugString".

---
+++ ExtensionMethods/Strings/ToDebugString.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethods.Strings
+{
+    /// <summary>
+    /// Provides extension methods for the <see cref="String"/> type.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns a debug string for the content of <paramref name="text"/> suitable for analyzing the string.
+        /// </summary>
+        /// <param name="text">The string to get a debug string for.</param>
+        /// <param name="maximumLineLength">The maximum number of characters each line in the returned string may have. Lines longer than that are wrapped.</param>
+        /// <returns>A debug string for the content of <paramref name="text"/> suitable for analyzing the string.</returns>
+        /// <example>
+        /// <code>
+        /// "ABC".ToDebugString();
+        /// </code>
+        /// Returns:
+        /// Character: |A|B|C|
+        /// Index:     |0|1|2|
+        /// </example>
+        /// <example>
+        /// <code>
+        /// "ABC\r\nEF".ToDebugString();
+        /// </code>
+        /// Returns:
+        /// Character: |A |B |C |\r|\n|E |F |
+        /// Index:     |00|01|02|03|04|05|06|
+        /// </example>
+        public static String ToDebugString(this String text, Int32 maximumLineLength = 80)
+        {
+            if (maximumLineLength < 25)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maximumLineLength), $"The argument {nameof(maximumLineLength)} must be equal to or greater than 25.");
+            }
+
+            if (String.IsNullOrEmpty(text))
+            {
+                return "The string is null or empty.";
+            }
+
+            var result = new StringBuilder();
+
+            var characters = text.ToCharArray();
+            var numberOfCharacters = characters.Length;
+            var numberOfDigitsInNumberOfCharacters = numberOfCharacters == 1 ? 1 : (Int32)Math.Floor(Math.Log10(numberOfCharacters) + 1);
+
+            var escapeSequences = characters.Select(a => GetEscapeSequence(a)).ToList();
+            var maximumEscapeSequenceLength = escapeSequences.Max(a => a.Length);
+            
+            var columnContentWidth = Math.Max(numberOfDigitsInNumberOfCharacters, maximumEscapeSequenceLength);
+            var columnWidth = columnContentWidth + 1 /* 1 for the pipe symbol in each column */;
+            var maximumNumberOfColumnsPerLine = (Int32) Math.Max(1, Math.Floor((maximumLineLength - 11 /* 11 for the prefix "Character: " */) / (Double) columnWidth));
+
+            var escapeSequencesChunks = Chunk(escapeSequences, maximumNumberOfColumnsPerLine);
+
+            var characterIndex = 0;
+            foreach (var escapeSequencesChunk in escapeSequencesChunks)
+            {
+                if (result.Length > 0)
+                {
+                    result.AppendLine();
+                }
+
+                result.Append("Character: |");
+                foreach (var escapeSequence in escapeSequencesChunk)
+                {
+                    result.Append(escapeSequence.PadRight(columnContentWidth));
+                    result.Append("|");
+                }
+                result.AppendLine();
+
+                result.Append("Index:     |");
+                for (var i = characterIndex; i < characterIndex + escapeSequencesChunk.Count; i++)
+                {
+                    result.Append(i.ToString(new String('0', columnContentWidth)));
+                    result.Append("|");
+                }
+                result.AppendLine();
+
+                characterIndex += escapeSequencesChunk.Count;
+            }
+
+            return result.ToString();
+        }
+
+        private static IEnumerable<List<T>> Chunk<T>(List<T> source, Int32 chunkSize)
+        {
+            for (int i = 0; i < source.Count; i += chunkSize)
+            {
+                yield return source.GetRange(i, Math.Min(chunkSize, source.Count - i));
+            }
+        }
+
+        private static String GetEscapeSequence(Char character)
+        {
+            return character switch
+            {
+                '\a' => "\\a",
+                '\b' => "\\b",
+                '\f' => "\\f",
+                '\n' => "\\n",
+                '\r' => "\\r",
+                '\t' => "\\t",
+                '\v' => "\\v",
+                <= (Char)254 => character.ToString(),
+                _ => "\\u" + ((Int32)character).ToString("X4")
+            };
+        }
+    }
+}
---
+++ ExtensionMethods/Strings/ToDebugStringTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using NUnit.Framework;
+using FluentAssertions;
+using static FluentAssertions.FluentActions;
+
+namespace ExtensionMethods.Strings
+{
+    [TestFixture(Description = "Tests for the ToDebugString extension method.")]
+    public class ToDebugStringTests
+    {
+        [Test(Description = "ToDebugString should throw an exception when the argument maximumLineLength is less than 25.")]
+        public void ToDebugString_InvalidMaximumLineLength_ShouldPrintCharacters()
+        {
+            Invoking(() => StringExtensions.ToDebugString("A", 24)).Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Test(Description = "ToDebugString should return 'The string is null or empty.' if the given string is null or empty.")]
+        public void ToDebugString_NullOrEmptyString_ShouldPrintCharacters()
+        {
+            StringExtensions.ToDebugString("").Should().Be(@"The string is null or empty.");
+        }
+
+        [Test(Description = "ToDebugString should print (printable) ASCII characters as they are.")]
+        public void ToDebugString_AsciiCharacters_ShouldPrintCharacters()
+        {
+            StringExtensions.ToDebugString("A").Should().Be(@"
+Character: |A|
+Index:     |0|
+".TrimStart());
+
+            StringExtensions.ToDebugString("ABC").Should().Be(@"
+Character: |A|B|C|
+Index:     |0|1|2|
+".TrimStart());
+
+            StringExtensions.ToDebugString("ABCDEFGHIJK").Should().Be(@"
+Character: |A |B |C |D |E |F |G |H |I |J |K |
+Index:     |00|01|02|03|04|05|06|07|08|09|10|
+".TrimStart());
+        }
+
+        [Test(Description = "ToDebugString should print control characters as escape sequences.")]
+        public void ToDebugString_ControlCharacters_ShouldPrintEscapeSequences()
+        {
+            StringExtensions.ToDebugString("\a\b\f\n\r\t\v").Should().Be(@"
+Character: |\a|\b|\f|\n|\r|\t|\v|
+Index:     |00|01|02|03|04|05|06|
+".TrimStart());
+        }
+
+        [Test(Description = "ToDebugString should print unicode characters as escape sequences.")]
+        public void ToDebugString_UnicodeCharacters_ShouldPrintUnicodeSequences()
+        {
+            StringExtensions.ToDebugString("\u9984\u9992\u9995\u9996").Should().Be(@"
+Character: |\u9984|\u9992|\u9995|\u9996|
+Index:     |000000|000001|000002|000003|
+".TrimStart());
+        }
+
+        [Test(Description = "ToDebugString should wrap lines longer than the specified maximum line length.")]
+        public void ToDebugString_MaximumLineLength_ShouldWrapLines()
+        {
+            StringExtensions.ToDebugString("01234567890123456789012345678901234567", 40).Should().Be(@"
+Character: |0 |1 |2 |3 |4 |5 |6 |7 |8 |
+Index:     |00|01|02|03|04|05|06|07|08|
+
+Character: |9 |0 |1 |2 |3 |4 |5 |6 |7 |
+Index:     |09|10|11|12|13|14|15|16|17|
+
+Character: |8 |9 |0 |1 |2 |3 |4 |5 |6 |
+Index:     |18|19|20|21|22|23|24|25|26|
+
+Character: |7 |8 |9 |0 |1 |2 |3 |4 |5 |
+Index:     |27|28|29|30|31|32|33|34|35|
+
+Character: |6 |7 |
+Index:     |36|37|
+".TrimStart());
+        }
+    }
+}
